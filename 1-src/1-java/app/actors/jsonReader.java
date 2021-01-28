f1b56ac3fd4dc8d6790f554b26f3ecaed3d8bdbd
Mark Glasgow <markglasgow@gmail.com>
2021-01-28 16:15:10 +0000

    Concurrency work - 48s
    Doesn't seem to be tripping up currently

--- 1-src/1-java/app/actors/jsonReader.java
+++ 1-src/1-java/app/actors/jsonReader.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.chen0040.embeddings.GloVeModel;
-import junit.framework.Test;
 import logic.Sanitise;
 import logic.SentimentAnalyzer;
 import logic.Twokenize;
@@ -18,12 +16,13 @@ import twitter.twittertext.TwitterTextParseResults;
 import twitter.twittertext.TwitterTextParser;
 
 import java.io.*;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -41,7 +40,9 @@ public class jsonReader {
     }
 
 
-    // This is the entry-point which passes each .jsonl file to parseEvent()
+    /**
+     * This method passes each file within the specified directory to parseEvent()
+     */
     public void parse() {
         try (Stream<Path> paths = Files.walk(Paths.get("../../0-data/raw/data/2020/2020-A/tweets/athens_earthquake"))) { //tweets/athens_earthquake  //testy
             paths.filter(Files::isRegularFile).forEach(jsonReader::parseEvent);
@@ -57,74 +58,97 @@ public class jsonReader {
      * Calculates the offset and event keywords before passing to tweetAnalyser()
      */
     public static void parseEvent(Path path)  {
+
+        // Ensure it's a selected.json file
         if(path.toString().contains("selected.jsonl") & !path.toString().matches(".*\\.gz")) {  //(".*\\.jsonl")
-            System.out.println("Path:" + path);
 
-            // Initialise actors for Word Embeddings & Sentiment Analysis
-            URL location = Test.class.getProtectionDomain().getCodeSource().getLocation();
-            String currentDirectory = System.getProperty("user.dir");
-            System.out.println("The current working directory is " + currentDirectory);
 
+            // Initialise actors for Word Embeddings
             //GloVeModel model = new GloVeModel();
             //model.load("lib/glove", 100);
 
-            SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
-            sentimentAnalyzer.initialize();
 
+
+            // is = a FileInputStream of the path
             try (InputStream is = new FileInputStream(String.valueOf(path))) {
+
+                // @lines = A Stream of strings
                 try (Stream<String> lines = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8)).lines()) {
+
+                    // Instantiate a new ObjectMapper object
                     ObjectMapper mapper = new ObjectMapper();
+
+                    // Configure the mapper to accept single values as arrays.
+                    // This is required so we can deserialise each line into an array
                     mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 
-                    // Iterate through each line in .jsonl and store it in a Tweet.class
+                    // Iterate through each line
                     for (String s : (Iterable<String>) lines::iterator) {
+
                         try {
+
+                            // @n = A JsonNode of the Tweet
                             JsonNode n = Json.parse(s);
+
+                            // @tweet = JsonNode mapped to the Tweet model using JacksonXMLs 'treeToValue'
                             Tweet tweet = mapper.treeToValue(n, Tweet.class); // here
+
+                            // @tweetList = An ArrayList we add all the to.
                             tweetList.add(tweet);
 
                         } catch (JsonProcessingException jsonProcessingException) {
+
+                            // json Processing exceptions will be printed to console
                             jsonProcessingException.printStackTrace();
                         }
 
                     }
 
-                    System.out.println("Preparing to parse " + tweetList.size() + " tweets from "  + path);
+                    System.out.println("\n" + tweetList.size() + " tweets read into model from" + path);
 
-                    // Keywords
-                    featureActor featureActor = new featureActor();
-                    featureActor.getKeywords(tweetList);
 
-                    // Offset + Sentiment + TwitterText + Glove
-                    tweetAnalyser(getMin(),  sentimentAnalyzer); //model,
-
-                    System.out.println("Parsed " + tweetList.size() + " tweets from "  + path);
-
-                    printVector("brand_new_run2");
                 }
 
 
             } catch (IOException e) {
                 System.out.println(e.toString());
-            }
 
+
+            }
         }
+        // Instantiate a new featureActor()
+        featureActor featureActor = new featureActor();
+
+        // getKeywords gets the TFIDF
+        featureActor.getKeywords(tweetList);
+
+        SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
+        sentimentAnalyzer.initialize();
+        // Offset + Sentiment + TwitterText + Glove
+        try {
+            tweetAnalyser(getMin(),  sentimentAnalyzer); //model,
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nParsed " + tweetList.size() + " tweets from "  + path);
+
+        printVector("brand_new_run2");
+
     }
 
 
     /**
+     * //@param model - word embeddings model
      * @param min - minimum time
-     * @param model - word embeddings model
      * @param sentimentAnalyzer - sentimentActor
-     * For each tweet in the event, hashtags, text-features are extracted
-     * The text is then sanitisied before sentiment analysis is performed.
-     * Then passed to NumericTweetFeatures() for exporting
+ * For each tweet in the event, hashtags, text-features are extracted
+ * The text is then sanitisied before sentiment analysis is performed.
      */
-    private static void tweetAnalyser(int min, SentimentAnalyzer sentimentAnalyzer) { //GloVeModel model,
+    private static void tweetAnalyser(int min, SentimentAnalyzer sentimentAnalyzer) throws InterruptedException { //GloVeModel model,
 
-        for(Iterator<Tweet> itr = tweetList.iterator(); itr.hasNext();){
-            Tweet tweet = itr.next();
-
+        //tweetList = tweetList.stream().map(x -> "D").collect(Collectors.toList());
+        for (Tweet tweet : new ArrayList<Tweet>(tweetList)) {
             final Extractor extractor = new Extractor();
             List<String> hashtags = extractor.extractHashtags(tweet.getText());
             tweet.setHashtags(hashtags);
@@ -152,7 +176,7 @@ public class jsonReader {
                 tweet.setPositiveSentiment(sentimentResult.getSentimentClass().getPositive());
                 tweet.setNegativeSentiment(sentimentResult.getSentimentClass().getNegative());
 
-            } catch(NullPointerException e){
+            } catch (NullPointerException e) {
                 e.printStackTrace();
                 continue;
             }
@@ -225,23 +249,23 @@ public class jsonReader {
         }
 
 
-        for(Iterator<Tweet> itr = tweetList.iterator(); itr.hasNext();){
-            Tweet tweet = itr.next();
-            double[] d = convertFloatsToDoubles(tweet.getDimensions());
-            if(tweet.getFeatureVector() != null){
+
+        for (Tweet tweet : tweetList) {
+            //double[] d = convertFloatsToDoubles(tweet.getDimensions());
+            if (tweet.getFeatureVector() != null) {
                 assert out != null;
 
                 // Print the feature vector
                 out.print(tweet.getFeatureVector());
 
-                /**
-                // Add the BERT Word Embeddings
-                out.print(",");
-                out.print("[");
-                for(double x : d){
-                    out.print(x + ", ");
-                }
-                out.print("]");
+                 /**
+                 // Add the BERT Word Embeddings
+                 out.print(",");
+                 out.print("[");
+                 for(double x : d){
+                 out.print(x + ", ");
+                 }
+                 out.print("]");
                  */
 
             }
